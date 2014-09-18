dd36f14c46a98470c7c59a7970c6c8ad90069629
Ian <ianacarr@gmail.com>
2014-09-18 18:40:01 -0400

    initial conditions in rocket flight

--- rocket-flight.py
+++ rocket-flight.py
@@ -1,3 +1,21 @@
 # Coding assignment: Rocket Flight
 # Ian Carr Sep 17 2014
 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from math import *
+
+# rocket parameters
+m_s = 50. # rocket shell mass (kg)
+C_D = 0.15 # coeff of drag
+m_po = 100. # inital mass of propellent (kg)
+v_e = 325. # speed of exhaust (m/s)
+r = 0.5 # radius of the rocket
+A = np.pi*r**2 # area of the rocket
+
+# environmental parameters
+g = 9.81 # acceleration due to grav (m/s**2)
+rho = 1.091 # air density in (kg/m**3)
+
+# time step
+dt = 0.1 # in seconds
