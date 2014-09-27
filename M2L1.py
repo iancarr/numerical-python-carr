44166950dfc0d852afe69f4533b211f422fd33b2
Ian <ianacarr@gmail.com>
2014-09-27 12:48:58 -0400

    Module 2 Lesson 1
    coded

---
+++ M2L1.py
@@ -0,0 +1,59 @@
+# Module 2 Lesson 1, Started Sep 25 2014
+# Ian Carr
+
+import numpy as np 
+import matplotlib.pyplot as plt
+
+# setting time and space parameters
+nx = 41 # number 
+dx = 2./(nx-1)
+nt = 10 
+dt = .02
+c = 1 # assuming wave speed of c = 1
+
+# ------- linear convection ---------
+
+# setting wave IC
+u = np.ones(nx) # creating space matrix
+u[.5/dx : 1/dx+1]=2 # setting u=2 between 0.5 and 1
+
+plt.figure()
+plt.plot(np.linspace(0,2,nx), u, ls='--', lw=3)
+plt.ylim(0,2.5)
+
+# setting up arrays to interate into
+un = np.ones(nx) # initializing array to interate into
+
+for n in range(nt):
+	un = u.copy()
+	for i in range(1,nx):
+		u[i] = un[i] - c*dt/dx*(un[i]-un[i-1])
+
+# plotting interated array
+plt.figure()
+plt.plot(np.linspace(0,2,nx), u)
+plt.ylim(0,2.5)
+
+
+# -------- non linear convection --------
+
+# using the same IC as linear
+u = np.ones(nx)
+u[.5/dx: 1/dx+1]=2
+
+un = np.ones(nx) # dummy array
+x = np.linspace(0,2,nx)
+
+plt.figure()
+plt.plot(np.linspace(0,2,nx), u)
+plt.ylim(0,2.5)
+
+for n in range(nt):
+	un = u.copy()
+	u[1:] = un[1:] - un[1:]*dt/dx*(un[1:]-u[0:-1])
+	u[0] = 1.0
+
+plt.figure()
+plt.plot(np.linspace(0,2,nx), u)
+plt.ylim(0,2.5)
+plt.show()
