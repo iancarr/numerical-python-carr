72aa66553f128b4c0ba1a72526fb34886b35e281
Ian <ianacarr@gmail.com>
2014-10-07 08:23:34 -0400

    Update animation 1D Diffusion

--- M2L3-1DDiffusion.py
+++ M2L3-1DDiffusion.py
@@ -31,10 +31,7 @@ plt.plot(np.linspace(0,2,nx),u)
 plt.ylim(0,2.5)
 
 # --------- Animations --------
-
-# importing animation libraries
-from JSAnimation.IPython_display import display_animation
-from matplotlib import animation
+plt.ion()
 
 # initial conditions for animation
 nt = 50
@@ -43,19 +40,22 @@ u[.5/dx : 1/dx+1] = 2
 
 un = np.ones(nx)
 
-# setting up figure and axes for animation
-fig = plt.figure(figsize=(8,5))
-ax = plt.axes(xlim=(0,2), ylim=(1,2.5))
-line = ax.plot([],[], ls='--', lw=3)[0]
-
 # defining func to solve for plots at each time
-def diffusion(i):
-	line.set_data(x,u)
-
+def diffusion(u):
 	un=u.copy()
 	u[1:-1] = un[1:-1] + nu*dt/dx**2*(un[2:]-2*un[1:-1]+\
 				un[0:-2])
+	return u
+# looping to plot animation
+for n in range(nt):
+	u = diffusion(u)
+	plt.plot(x,u)
+	plt.pause(0.01)
+	plt.clf()
+	plt.xlim([0,2])
+	plt.ylim([0,2])
 
-animation.FuncAnimation(fig,diffusion,frames=nt,interval=100)
+
+
 
 plt.show()
